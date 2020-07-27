fb8ffc4aabe4dfbeb1b356230d6031397a240c58
Leon Peng <g70245@gmail.com>
2020-07-27 18:32:24 +0800

    skip access control when using executeGraphQL

--- helpers/createDefaultAdmin.js
+++ helpers/createDefaultAdmin.js
@@ -19,12 +19,14 @@ module.exports = project => async keystone => {
     );
 
     if (count === 0) {
+        const context = keystone.createContext({ skipAccessControl: true });
         const projectAdminRole = project === 'mirrormedia' ? 'role: "moderator", isAdmin: true' : 'role: "admin"';
         const email = 'admin@mirrormedia.mg';
         const password = 'mirrormedia';
         // const password = (process.env.NODE_ENV === 'development') ? 'mirrormedia' : randomString();
 
         await keystone.executeGraphQL({
+            context,
             query: gql`mutation initialUser($password: String, $email: String) {
                 createUser(data: {name: "admin", email: $email, password: $password, ${projectAdminRole}}) {
                 id
