5c231e1c871c6d7af6fc200aca9c974c485792f1
“liyibass” <liyibass@mirrormedia.mg>
2020-12-17 17:09:34 +0800

    refactor: revert HTML field Controller path, update draft version.

--- fields/HTML/views/Field.js
+++ fields/HTML/views/Field.js
@@ -1,7 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { FieldContainer, FieldLabel, FieldDescription } from '@arch-ui/fields'
 import HtmlDraftEditor from '@liyibass/html-draft-editor'
-
+import {
+    BlockMapBuilder,
+    Editor,
+    EditorState,
+    KeyBindingUtil,
+    Modifier,
+    Entity,
+    RichUtils,
+    convertFromHTML,
+    convertFromRaw,
+    convertToRaw,
+    getDefaultKeyBinding,
+} from 'draft-js'
 const HtmlField = ({ onChange, autoFocus, field, value, errors }) => {
     return (
         <FieldContainer>
