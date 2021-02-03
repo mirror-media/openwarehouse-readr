493676724d3ed782ec0602da9a42d329f38dfb7c
“liyibass” <liyibass@mirrormedia.mg>
2021-02-03 17:42:21 +0800

    feat: update draft editor's version(fix gql title-to-name adjust)

--- helpers/createDefaultAdmin.js
+++ helpers/createDefaultAdmin.js
@@ -3,6 +3,7 @@ const randomString = () => crypto.randomBytes(6).hexSlice()
 
 module.exports = (project) => async (keystone) => {
     // Count existing users
+    // However, this query don't have access (TODO)
     const {
         data: {
             _allUsersMeta: { count },
@@ -24,9 +25,10 @@ module.exports = (project) => async (keystone) => {
         const email = 'admin@mirrormedia.mg'
         const password = 'mirrormedia'
         // const password = (process.env.NODE_ENV === 'development') ? 'mirrormedia' : randomString();
+        console.log('------ready to create new admin user------')
 
-        await keystone.executeGraphQL({
-            mutation: `mutation initialUser($password: String, $email: String) {
+        const response = await keystone.executeGraphQL({
+            query: `mutation initialUser($password: String, $email: String) {
                 createUser(data: {name: "admin", email: $email, password: $password, ${projectAdminRole}}) {
                 id
                 }
@@ -36,6 +38,8 @@ module.exports = (project) => async (keystone) => {
                 email,
             },
         })
+
+        console.log(response)
 
         console.log(`
             User created:
